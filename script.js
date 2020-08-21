8fb5416ef514ab1202bc1a46a47f7db0b8f0df15
Manuela Scotto <manuelascotto18@gmail.com>
2020-08-21 13:18:05 +0200

    completato

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+//////////////// COSTANTI ////////////////
 const newLesson = document.getElementById('formNewLesson');
 const inputNew = document.getElementById('newLesson');
 const filter = document.getElementById('filter');
 const btnDelete = document.getElementById('btn-delete');
 const ulList = document.getElementById('list');
 
+//////////////// EVENTI ////////////////
 newLesson.addEventListener('submit', addLesson);
 ulList.addEventListener('click', removeLesson);
 filter.addEventListener('keyup', addFilter);
 btnDelete.addEventListener('click', deleteAll);
 
+//////////////// CALLBACK ////////////////
+// aggiungi 
 function addLesson(e) {
     if (inputNew.value == '') {
         alert('Non hai inserito l\'input')
     } else {
-        console.log(inputNew.value);
         let item = document.createElement('li');
         item.appendChild(document.createTextNode(inputNew.value.toLowerCase()));
         ulList.appendChild(item);
@@ -27,24 +30,25 @@ function addLesson(e) {
     e.preventDefault();
 }
 
+// rimuovi singolo elemento da tag fantasma
 function removeLesson(e) {
     if (e.target.classList.contains('close')) {
         e.target.parentElement.parentElement.remove();
     }
 }
 
+// ricerca 
 let li = ulList.getElementsByTagName('li');
 function addFilter(e) {
     let f = e.target.value.toLowerCase();
-    console.log(f);
     console.log(li);
     Array.from(li).forEach(e => {
         let liValue = (e.firstChild.textContent);
-        console.log(liValue);
         liValue.includes(f) ? e.style.display = 'block' : e.style.display = 'none';
     })
 }
 
+// cancella tutto
 function deleteAll() {
     Array.from(li).forEach(e => {
         e.remove();
